d847e0e8f7e73ff43f06f8000ac143396a7db58a
Ayoub Maatouch <contact@maatouch.com>
2022-10-02 23:03:05 +0100

    importing app to build-machine

---
+++ services/build/app/index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+    <h1>
+        <?php echo 'youcan.shop is server now by ', getHostByName(getHostName()); ?> 
+    </h1>
+ </body>
+</html>
